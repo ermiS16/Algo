f00ff3f82bb88d7ccd6a109ef1f4a94f678cfe33
Eric Misfeld <eric_misf@gmx.de>
2018-11-30 16:28:29 +0100

    Changed DoubleLinkedList to use ForEach instead of Iterator

--- src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
+++ src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
@@ -1,7 +1,5 @@
 package de.hska.iwi.ads.solution.list;
 
-import java.util.Iterator;
-
 import de.hska.iwi.ads.dictionary.*;
 
 public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDoubleLinkedList<K, V> {
@@ -9,18 +7,20 @@ public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDouble
 	@Override
 	public V get(Object o) {
 		ListElement current = this.head;
+		Entry<K,V> entry;
 		if (o == null) {
 			throw new NullPointerException();
 		}
 		@SuppressWarnings("unchecked")
 		K key = (K) o;
 
-		current = getElement(current, key);
-		if (current != null) {
-			return current.entry.getValue();
-		} else {
+		entry = getElement(current, key);
+		if(entry != null) {
+			return entry.getValue();	
+		}else {
 			return null;
 		}
+		
 	}
 
 	/**
@@ -28,19 +28,15 @@ public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDouble
 	 * 
 	 * @param current ListElement from which the iteration starts.
 	 * @param key     to Compare Elements.
-	 * @return ListElement with the specific key. Null when key not exist.
+	 * @return Entry<K,V> with the specific key. Null when key not exist.
 	 */
 
-	private ListElement getElement(ListElement current, K key) {
-		
-		Iterator<Entry<K, V>> iter = this.iterator();
-		while (iter.hasNext()) {
-			if (current.entry.getKey().compareTo(key) == 0) {
-				return current;
-			} else {
-				current = current.next;
+	private Entry<K, V> getElement(ListElement current, K key) {
+
+		for(Entry<K,V> entry : this.entrySet()){
+			if(entry.getKey().compareTo(key) == 0) {
+				return entry;
 			}
-			iter.next();
 		}
 		return null;
 	}
@@ -48,28 +44,30 @@ public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDouble
 	@Override
 	public V put(K key, V value) {
 		ListElement current = this.head;
+		Entry<K,V> currentEntry;
 		V oldValue;
 
 		if (key == null) {
 			throw new NullPointerException();
 		}
 
-		if (this.head == null) {
-			this.head = new ListElement(new SimpleEntry<>(key, value), null, null);
-			this.size++;
-			return null;
-		} else {
-			current = getElement(current, key);
-			if (current != null) { // Replace when Key already exist
-				oldValue = current.entry.getValue();
-				current.entry.setValue(value);
+		currentEntry = getElement(current, key);
+		if(current != null) {
+			if (currentEntry != null) { // Replace when Key already exist
+				oldValue = currentEntry.getValue();
+				currentEntry.setValue(value);
 				return oldValue;
 			} else { // Add new Element if Key not exist
 				this.head.previous = new ListElement(new SimpleEntry<>(key, value), null, this.head);
 				this.head = this.head.previous;
 				this.size++;
 				return null;
-			}
+			}		
+		}else {
+			this.head = new ListElement(new SimpleEntry<>(key, value), null, null);
+			this.size++;
+			return null;
 		}
+			
 	}
 }
