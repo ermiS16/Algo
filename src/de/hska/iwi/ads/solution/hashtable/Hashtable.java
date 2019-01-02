192affb9927aa1120c251db0f6829092f3ab9383
Eric Misfeld <eric_misf@gmx.de>
2019-01-03 00:14:01 +0100

    hashtable complete. now for real

--- src/de/hska/iwi/ads/solution/hashtable/Hashtable.java
+++ src/de/hska/iwi/ads/solution/hashtable/Hashtable.java
@@ -7,10 +7,10 @@ public class Hashtable<K extends Comparable<K>, V> extends AbstractHashMap<K,V>{
 		super(capacity);
 	}
 	
-
 	@Override
 	public V get(Object o) {
-		Entry<K,V> entry;
+		int index = 0;
+		int hash;
 		
 		if(o == null) {
 			throw new NullPointerException();
@@ -18,74 +18,48 @@ public class Hashtable<K extends Comparable<K>, V> extends AbstractHashMap<K,V>{
 		@SuppressWarnings({ "unchecked" })
 		K key = (K) o;
 		
-		entry = getEntry(key);
-		if(entry != null) {
-			return entry.getValue();	
-		}else {
-			return null;
-		}
-	}
-	
-	/**
-	 * Iterates through the hashtable
-	 * @param key to Compare Entrys
-	 * @return The entry with the specific key. Null when key not exist.
-	 */
-	private Entry<K,V> getEntry(K key){
-//		int hash = key.hashCode();
-		
-		for(Entry<K,V> entry : this.entrySet()){
-			if(entry.getKey().compareTo(key) == 0) {
-				return entry;
+		do {
+			hash = quadraticProbe(key, index);
+			if(hashtable[hash] != null) {
+				if(hashtable[hash].getKey().compareTo(key) == 0) {
+					return hashtable[hash].getValue();
+				}				
 			}
-		}
+			index++;
+		}while(hashtable[hash] != null && index < hashtable.length);
+		
 		return null;
 	}
 	
-	private int quadraticProbe(int hash, int counter) {
-		int newHash = hash;
+	private int quadraticProbe(K key, int counter) {
+		int hash = key.hashCode() % hashtable.length;
 		
-		if(newHash < 0) {
-			newHash *= -1;
-		}
-		
-//		while(counter < hashtable.length || hashtable[counter] != null) {
-			newHash = (newHash + (counter*counter))%hashtable.length;	
-//		}
-		
-		return newHash;
+		hash = (hash + (counter*counter))%hashtable.length;
+		if(hash < 0) hash *= -1;
+		return hash;
 	}
 	
 	@Override
 	public V put(K key, V value) {
 		V oldValue = null;
 		int index = 0;
-		int hash = key.hashCode()%hashtable.length;
-		if(hash < 0) {
-			hash *= -1;
-		}
-		Entry<K,V> current = getEntry(key);
-		
-		
-		if(current != null) {
-			oldValue = current.getValue();
-			current.setValue(value);
-			return oldValue;
-		}
-		if(size >= hashtable.length ){
-			throw new DictionaryFullException();
-		}else{
-			while(index<hashtable.length) {
-				hash = quadraticProbe(hash, index);
-				if(hashtable[hash] == null) {
-					current = new SimpleEntry<>(key, value);
-					hashtable[hash] = current;
-					this.size++;
-					return null;
+		int hash;
+
+		do {
+			hash = quadraticProbe(key, index);			
+			if(hashtable[hash] == null) {
+				hashtable[hash] = new SimpleEntry<>(key, value);
+				this.size++;
+				return null;
+			}else {
+				if(hashtable[hash].getKey().compareTo(key) == 0){
+					oldValue = hashtable[hash].getValue();
+					hashtable[hash].setValue(value);
+					return oldValue;
 				}
-				index++;
 			}
-		}
-		return null;
+			index++;
+		}while(index < hashtable.length);	
+		throw new DictionaryFullException();
 	}
 }
